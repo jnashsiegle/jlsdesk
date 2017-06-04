a6327f3ec287ac7bb4c71bbf79dba2f6b174de7d
jnashsiegle <jsiegle@fullsail.edu>
2017-06-04 22:07:41 +0000

    fixed portfolio

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -23,8 +23,7 @@
 				</div>
 				<div class="col-lg-6">
 					<p><a href = "http://lightpointdev.com" target = "_blank">LightPoint Development</a></p>
-				<p><more>There was also a hidden CMS backend written from scratch using Auth0 for theAdministration Admin Area back end built to accomodate administrative and authorized user roles. Allowing authentication of each role upon login and allowing access to only certain areas dependant upon which role is logged in and prohibiting access from guests. The administrator creates the users, assigns the roles and can delete the roles. All roles had access to the image panel and pages and could upload, edit and delete images, captions and descriptions, as well as (forgotten) password reset capability.</more></p>
-				<p>It is also hosted on Digital Ocean where I set up the server configuration using Nginx and Laravel Forge using Git as part of the deployment workflow.</p>
+				<p><more>A Web Development Agency focusing on helping non-profits, charities, ministries and more with their online presences.</more></p>				
 				<p><a href = "http://lightningroad.org/colophon" target = "_blank">Project Colophon</a></p>
 				
 					<sm><i class="icon-tag"></i> design and development</sm></more> 
