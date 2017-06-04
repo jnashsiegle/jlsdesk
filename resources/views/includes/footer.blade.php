9a4ef472402933f6dfdbaeb88af2a3e2847772bc
jnashsiegle <jsiegle@fullsail.edu>
2017-06-04 22:04:02 +0000

    tweaks

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -6,19 +6,19 @@
     	<div class = "flex-container">
     	<div class = "flex-item">
     		<p>Copyright <i class="fa fa-copyright" aria-hidden="true"></i>  
-		<?php echo date("Y"); ?> LightPoint Development, LLC.  All rights reserved.<br /><a href = "http://lightpointdev.com">LightPoint Development</a></p>
+		<?php echo date("Y"); ?> LightPoint Development, LLC.  All rights reserved.<br /><a href = "http://lightpointdev.com" target = "_blank">LightPoint Development</a></p>
  	</div>   
     	<div class = "flex-item">   
 	   <p class="links" id = "social">
-	     <a href="https://twitter.com/JLSiegle" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @JLSiegle</a>
+	     <a href="https://twitter.com/JLSiegle" class="twitter-follow-button" data-show-count="false" data-size="large" target = "_blank">Follow @JLSiegle</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script><br />
-	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Behance - Check out my artwork!"><a href="https://www.behance.net/jsiegle048a"><i class="fa fa-behance fa-3x" aria-hidden="true" ></i></a></span>
-	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Codepen- One of my Snippet Collections"><a href = "http://codepen.io/jsiegle/"><i class="fa fa-codepen fa-3x" aria-hidden="true"></i></a></span>
-	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="DeviantArt - Check out my artwork!!"><a href="http://jana-siegle.deviantart.com/"><i class="fa fa-deviantart fa-3x" aria-hidden="true"></i></a></span>
-	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Facebook"><a href="https://www.facebook.com/jana.siegle.9"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a></span>
-	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="All my repos on GitHub"><a href="https://github.com/jnashsiegle"><i class="fa fa-github fa-3x" aria-hidden="true" ></i></a></span>
-	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Check out my skills and certifications | experience and more on LinkedIn!"><a href="https://www.linkedin.com/in/janasiegle"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a></span>
-	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="@JLSiegle on Twitter!"><a href="https://twitter.com/JLSiegle"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a></span>
+	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Behance - Check out my artwork!"><a href="https://www.behance.net/jsiegle048a" target = "_blank"><i class="fa fa-behance fa-3x" aria-hidden="true" ></i></a></span>
+	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Codepen- One of my Snippet Collections"><a href = "http://codepen.io/jsiegle/" target = "_blank"><i class="fa fa-codepen fa-3x" aria-hidden="true"></i></a></span>
+	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="DeviantArt - Check out my artwork!!"><a href="http://jana-siegle.deviantart.com/" target = "_blank"><i class="fa fa-deviantart fa-3x" aria-hidden="true"></i></a></span>
+	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Facebook"><a href="https://www.facebook.com/jana.siegle.9" target = "_blank"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a></span>
+	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="All my repos on GitHub"><a href="https://github.com/jnashsiegle" target = "_blank"><i class="fa fa-github fa-3x" aria-hidden="true" ></i></a></span>
+	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Check out my skills and certifications | experience and more on LinkedIn!"><a href="https://www.linkedin.com/in/janasiegle" target = "_blank"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a></span>
+	     <span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="@JLSiegle on Twitter!"><a href="https://twitter.com/JLSiegle" target = "_blank"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a></span>
 	     <a href="tel:&#56;&#56;&#56;&#45;&#51;&#53;&#51;&#45;&#55;&#53;&#51;&#49;"><span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Call us now!"><i class="fa fa-phone fa-3x" aria-hidden="true"></i>
 	      	</span></a></p>
      </div><!-- end flex item -->
--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -12,7 +12,7 @@
 					<p><a href = "http://lightningroad.org" target = "_blank">Lightning Road Biker Outreach</a></p>
 				<p><more>There was also a hidden CMS backend written from scratch using Auth0 for theAdministration Admin Area back end built to accomodate administrative and authorized user roles. Allowing authentication of each role upon login and allowing access to only certain areas dependant upon which role is logged in and prohibiting access from guests. The administrator creates the users, assigns the roles and can delete the roles. All roles had access to the image panel and pages and could upload, edit and delete images, captions and descriptions, as well as (forgotten) password reset capability.</more></p>
 				<p>It is also hosted on Digital Ocean where I set up the server configuration using Nginx and Laravel Forge using Git as part of the deployment workflow.</p>
-				<p><a href = "http://lightningroad.org/colophon">Project Colophon</a></p>
+				<p><a href = "http://lightningroad.org/colophon" target = "_blank">Project Colophon</a></p>
 				
 					<sm><i class="icon-tag"></i> design and development</sm></more> 
 				</p>
@@ -25,7 +25,7 @@
 					<p><a href = "http://lightpointdev.com" target = "_blank">LightPoint Development</a></p>
 				<p><more>There was also a hidden CMS backend written from scratch using Auth0 for theAdministration Admin Area back end built to accomodate administrative and authorized user roles. Allowing authentication of each role upon login and allowing access to only certain areas dependant upon which role is logged in and prohibiting access from guests. The administrator creates the users, assigns the roles and can delete the roles. All roles had access to the image panel and pages and could upload, edit and delete images, captions and descriptions, as well as (forgotten) password reset capability.</more></p>
 				<p>It is also hosted on Digital Ocean where I set up the server configuration using Nginx and Laravel Forge using Git as part of the deployment workflow.</p>
-				<p><a href = "http://lightningroad.org/colophon">Project Colophon</a></p>
+				<p><a href = "http://lightningroad.org/colophon" target = "_blank">Project Colophon</a></p>
 				
 					<sm><i class="icon-tag"></i> design and development</sm></more> 
 				</p>
