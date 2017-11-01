ca9d00ab1df184452495f9a3a7bde2a05bf58716
Jana Siegle <janasiegle@Janas-MacBook-Pro.local>
2017-11-01 14:23:04 -0500

    adding landing page

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,10 @@
+<div id="headerwrap">
+    <div class="container">
+      <div class="row centered">
+        <div class="col-lg-12">
+          <h1>Jana Nash-Siegle</h1>
+          <h2>Web Application Developer</h2>
+        </div><!--/.col-lg-12 -->
+      </div><!--/.row -->
+    </div><!--/.container -->
+  </div><!--/.#headerwrap -->
